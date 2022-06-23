ca689cb4880974502227716e739f56770e7d7ce5
Kawana T Small <jazzytech@Kawanas-MacBook-Pro.local>
2022-06-22 20:36:58 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers) {
+  return drivers.slice(0, 2);
+};
+const returnLastTwoDrivers = function (drivers){
+  return drivers.slice(-2);
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fareMultiplier) {
+  return function (fare) {
+    return fareMultiplier * fare;
+  };
+}
+const fareDoubler = function (fare) {
+  return fare * 2;
+}
+const fareTripler = function (fare){
+  return fare * 3;
+}
+function selectDifferentDrivers(arrayOfDrivers, returnFirstTwoDrivers){
+  return returnFirstTwoDrivers(arrayOfDrivers)
+  }
+  
+
+
+
+
+
+
+
+
+/* Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers) {
+  return drivers.slice(0, 2);
+};
+const returnLastTwoDrivers = function (drivers) {
+  return drivers.slice(-2);
+};
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fareMultiplier) {
+  return function (fare) {
+    return fareMultiplier * fare;
+  };
+}
+const fareDoubler = function (fare) {
+  return fare * 2;
+};
+const fareTripler = function (fare) {
+  return fare * 3;
+};
+
+function selectDifferentDrivers(arrayOfDrivers, drivers) {
+  console.log(arrayOfDrivers, drivers);
+  const newDrivers = drivers(arrayOfDrivers);
+  console.log(newDrivers);
+  return newDrivers;
+}*/
